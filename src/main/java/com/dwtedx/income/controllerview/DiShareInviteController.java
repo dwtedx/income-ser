0565de90a3595c7ae29ef70fa0a71bd47bebf4f6
shinyuu <dwtedx@qq.com>
2020-03-25 17:42:52 +0800

    handler

--- src/main/java/com/dwtedx/income/controllerview/DiShareInviteController.java
+++ src/main/java/com/dwtedx/income/controllerview/DiShareInviteController.java
@@ -3,7 +3,6 @@ package com.dwtedx.income.controllerview;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
