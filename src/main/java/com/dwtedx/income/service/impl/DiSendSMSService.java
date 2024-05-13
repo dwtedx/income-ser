c7c0c7fc764b7734fc0ec0cb2e435924e457365c
shinyuu <qinyoulong@szaiten.com>
2024-05-13 13:35:05 +0800

    feat(*): DD记账 Java后端开源

--- src/main/java/com/dwtedx/income/service/impl/DiSendSMSService.java
+++ src/main/java/com/dwtedx/income/service/impl/DiSendSMSService.java
@@ -126,8 +126,8 @@ public class DiSendSMSService implements IDiSendSMSService {
 		final String product = "Dysmsapi";//短信API产品名称（短信产品名固定，无需修改）
 		final String domain = "dysmsapi.aliyuncs.com";//短信API产品域名（接口地址固定，无需修改）
 		//替换成你的AK
-		final String accessKeyId = "LTAICiSbu3VSQL1a";//你的accessKeyId,参考本文档步骤2
-		final String accessKeySecret = "9jHwdTJka3DhShIvDa5RiVswAt4Kv0";//你的accessKeySecret，参考本文档步骤2
+		final String accessKeyId = "";//你的accessKeyId,参考本文档步骤2
+		final String accessKeySecret = "";//你的accessKeySecret，参考本文档步骤2
 		//初始化ascClient,暂时不支持多region（请勿修改）
 		IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId,
 		accessKeySecret);
